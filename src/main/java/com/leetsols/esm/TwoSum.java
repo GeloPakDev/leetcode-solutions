58e93fb95b7080509d51d82b83b67494fe698464
GeloPakDev <pakolegdev@gmail.com>
2025-06-03 22:53:05 +0500

    Initial commit

---
+++ src/main/java/com/leetsols/esm/TwoSum.java
@@ -0,0 +1,13 @@
+package com.leetsols.esm;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        var map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+        }
+        return result;
+    }
+}
